d051371f81fe1f41c6971c41016dba5d732a556b
Ibraheem Akbar <121730444+Akbarable@users.noreply.github.com>
2023-12-24 20:37:32 +0500

    Create LSTM_model.py

---
+++ models/LSTM_model.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+class LSTMModel(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, num_layers=2):
+        super(LSTMModel, self).__init__()
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.lstm = nn.LSTM(hidden_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        embedded = self.embedding(x)
+        output, _ = self.lstm(embedded)
+        output = self.fc(output)
+        return output
