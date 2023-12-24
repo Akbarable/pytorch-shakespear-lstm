fdf89b63df43bb9db05aded999df894cc1c5310d
Ibraheem Akbar <121730444+Akbarable@users.noreply.github.com>
2023-12-24 21:13:31 +0500

    Update train.py

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import torch
 from torch.utils.data import DataLoader
 from utils.dataset import ShakespeareDataset
 from models.LSTM_model import LSTMModel
+import matplotlib.pyplot as plt
 
 #dataset
 dataset = ShakespeareDataset("data/input.txt", sequence_length=30)
@@ -23,17 +24,39 @@ model = LSTMModel(input_size, hidden_size, output_size, num_layers)
 criterion = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
+# Lists to store training loss for each epoch
+train_loss_history = []
+
 # Training loop
 for epoch in range(num_epochs):
-    for batch_sequence, batch_target in dataloader:
+    epoch_loss = 0.0
+    num_batches = len(dataloader)
+    
+    for i, (batch_sequence, batch_target) in enumerate(dataloader):
         optimizer.zero_grad()
         output = model(batch_sequence)
         loss = criterion(output, batch_target.long())  # Convert to torch.long
         loss.backward()
         optimizer.step()
+        
+        epoch_loss += loss.item()
 
-    print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item()}')
+        # Print progress every 100 batches
+        if (i + 1) % 100 == 0 or (i + 1) == num_batches:
+            print(f'Epoch [{epoch+1}/{num_epochs}], Batch [{i+1}/{num_batches}], Loss: {loss.item()}')
 
+    # Average loss for the epoch
+    avg_epoch_loss = epoch_loss / num_batches
+    train_loss_history.append(avg_epoch_loss)
+
+    print(f'Epoch [{epoch+1}/{num_epochs}], Average Loss: {avg_epoch_loss}')
+
+# Plotting the training loss over epochs
+plt.plot(train_loss_history, label='Training Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
 
 # Save the trained model
 torch.save(model.state_dict(), 'shakespeare_lstm_model.pth')
